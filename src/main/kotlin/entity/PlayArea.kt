3cadd2c48c9969cf24ee57eedeec6ca67060c24d
Zeyad Eissa <un9$mco9>
2021-08-24 20:52:34 +0200

    Entity_model

--- src/main/kotlin/entity/PlayArea.kt
+++ src/main/kotlin/entity/PlayArea.kt
@@ -2,7 +2,6 @@ package entity
 /**
  * An Entity that conncets both the Deck and the cards on the table together.
  */
-class PlayArea {
-    val deck:Deck?=null
-    val cards = mutableListOf<Card>()
+class PlayArea(var deck: Deck, var cards: MutableList<Card>) {
+
 }
